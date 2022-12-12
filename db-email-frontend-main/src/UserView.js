32cf12f298398b922ecdfb68a9d8d1e29af3cd18
AgustinBarreto <barretoagustin5@gmail.com>
2022-12-12 05:44:55 -0400

    Changed date of birth in register and added log out button

--- db-email-frontend-main/src/UserView.js
+++ db-email-frontend-main/src/UserView.js
@@ -1,5 +1,6 @@
 import React, {Component, useEffect, useState} from 'react';
 import {Button, Card, Container, Divider, Header, Icon, Image, Modal, Tab} from "semantic-ui-react";
+import {Routes, Route, useNavigate} from 'react-router-dom';
 import Dashboard from "./Dashboard";
 import Emails from "./Emails";
 import Account from "./Account";
@@ -11,11 +12,15 @@ var U =3
 function UserView(){
     const [open, setOpen] = useState(false);
     console.log(open);
-
+    const navigate = useNavigate();
     const [isAuth, setIsAuth] = useState(true)
     const [UpdateTab, setUpdateTab] = useState(false)
 
-
+    const navigateToLogin = () => {
+        // 👇️ navigate to /Login
+        localStorage.clear()
+        navigate('/Login');
+      };
     const UpdateData = () => {
         localStorage.setItem('Change:','true')
         console.log('I entered here')
@@ -35,6 +40,9 @@ function UserView(){
         },
         {
             menuItem: 'Dashboard', render: () => <Tab.Pane active={isAuth}><Dashboard /></Tab.Pane>
+        },
+        {
+            menuItem: 'Log Out', render: () => <Header>Do you want to log out? <Button onClick={navigateToLogin}>Yes</Button> <Button>No</Button></Header>
         }
     ]
 
